cea3c20adfae237d9b30776ba62b150295a4055e
Darryl T. Agostinelli <dagostinelli@gmail.com>
2019-07-06 19:10:37 -0500

    cleanup experimental tests

--- test/test_experimental.c
+++ test/test_experimental.c
@@ -18,33 +18,9 @@ static char *test_quaternion_get_eulers_create_quaternion_ZYX(void)
 	return 0;
 }
 
-
-static char *test_quaternion_get_eulers_create_quaternion_ZYX_aa(void)
-{
-	struct quaternion q1, q2;
-	HYP_FLOAT anglex, angley, anglez;
-
-	/* making the original quaternion out of an arbitrary axis angle */
-	quaternion_set_from_axis_anglef3(&q1, 0.4f, 0.232f, 0.543f, HYP_TAU / 1.45f);
-
-	/* get the angles */
-	quaternion_get_euler_anglesf3_ZYX_EXP(&q1, &anglex, &angley, &anglez);
-
-	/* compose new quaternions with the eulers */
-	quaternion_set_from_euler_anglesf3_ZYX_EXP(&q2, anglex, angley, anglez);
-
-	/* same */
-	test_assert(quaternion_equals(&q1, &q2));
-
-	return 0;
-}
-
-
-static char *experimental_all_tests()
+static char *experimental_all_tests(void)
 {
 	run_test(test_quaternion_get_eulers_create_quaternion_ZYX);
-	run_test(test_quaternion_get_eulers_create_quaternion_ZYX_aa);
 
 	return 0;
 }
-
