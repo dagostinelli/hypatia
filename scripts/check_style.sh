21d43a1c8b3ae791d9c0b8cafeae98a20db3ae1d
Darryl T. Agostinelli <dagostinelli@gmail.com>
2020-01-12 08:21:34 -0600

    ignore SPDX comments and paren alignment false-positives

--- scripts/check_style.sh
+++ scripts/check_style.sh
@@ -6,6 +6,6 @@ cd $(dirname $(realpath -s "$BASH_SOURCE"))
 
 cd ..
 
-find . -type f | grep -v -e ".txt" -e ".git" -e "build/" -e "install/" | xargs /usr/src/kernels/`uname -r`/scripts/checkpatch.pl -f --no-tree --no-summary --terse --show-types --subjective --ignore LONG_LINE,OPEN_BRACE,ELSE_AFTER_BRACE,MACRO_WITH_FLOW_CONTROL,CAMELCASE,LINE_SPACING,SPACING,PREFER_KERNEL_TYPES,MACRO_ARG_REUSE
+find . -type f | grep -v -e ".txt" -e ".git" -e "build/" -e "install/" | xargs /usr/src/kernels/`uname -r`/scripts/checkpatch.pl -f --no-tree --no-summary --terse --show-types --subjective --ignore LONG_LINE,OPEN_BRACE,ELSE_AFTER_BRACE,MACRO_WITH_FLOW_CONTROL,CAMELCASE,LINE_SPACING,SPACING,PREFER_KERNEL_TYPES,MACRO_ARG_REUSE,PARENTHESIS_ALIGNMENT,SPDX_LICENSE_TAG
 
 popd
